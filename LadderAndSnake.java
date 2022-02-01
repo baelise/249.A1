aa68c72344047ee24573d3cedbdfa6fd619d539b
baelise <94066054+baelise@users.noreply.github.com>
2022-02-01 17:32:31 -0500

    Update LadderAndSnake.java

--- LadderAndSnake.java
+++ LadderAndSnake.java
@@ -28,72 +28,72 @@ public class LadderAndSnake {
     	return roll;
     }
     public void play() {
-    //STILL NEED TO RESET I and J
-    boolean playing = true;
-    boolean checkLS = true;
-    boolean reg = true;
-	int[] positions = {0, 0, 0, 0};
-    while(playing) {
-    	for(int i = 0; i < getPlayers(); i++) {
-    		checkLS = true;
-    	    reg = true;
-    	    int nextRoll = flipDice();
-			System.out.println("POSITIONS: " + positions[0] + ", " + positions[1]+ ", " + positions[2]+ ", " + positions[3]); 
-			System.out.println("ROLL for player " + i + " is " + nextRoll);
-			while(checkLS) {
-				System.out.println("Checkin LS");
-        		for(int j = 0; j <15; j++) {
-        			if(positions[i]+nextRoll == board[j][0]) {
-            			positions[i] = board[j][1];
-            			checkLS = false;
-                		reg = false;
-            			break;
-            		}
-        		}
-        		checkLS = false;
-    		}
-			while(reg)
-			{
-				System.out.println("In regular");
-				//positions[i] = positions[i] + nextRoll;
-				if(positions[i]+nextRoll > 100) {
-					int excess = 100-(positions[i] + nextRoll);
-					System.out.println("*****OVER 100*****");
-					if(100+excess == 98 ) {
-						positions[i] = 78;
-						reg = false;
-					}
-					if(100+excess == 98) {
-						positions[i] = 76;
-						reg = false;
-					}
-					if(100+excess == 95) {
-						positions[i] = 24;
-						reg = false;
-					}
-					else {
-						positions[i] = 100+excess;
-						reg = false;
-					}
-				}
-				if(positions[i]+nextRoll==100 || positions[i]+nextRoll==80) {
-					positions[i] = 100;
-					System.out.println("Player " + i + " has won ");
-					reg = false;
+	    //STILL NEED TO RESET I and J
+	    boolean playing = true;
+	    boolean checkLS = true;
+	    boolean reg = true;
+		int[] positions = {0, 0, 0, 0};
+	    while(playing) {
+		for(int i = 0; i < getPlayers(); i++) {
+			checkLS = true;
+		    reg = true;
+		    int nextRoll = flipDice();
+				System.out.println("POSITIONS: " + positions[0] + ", " + positions[1]+ ", " + positions[2]+ ", " + positions[3]); 
+				System.out.println("ROLL for player " + i + " is " + nextRoll);
+				while(checkLS) {
+					System.out.println("Checkin LS");
+				for(int j = 0; j <15; j++) {
+					if(positions[i]+nextRoll == board[j][0]) {
+					positions[i] = board[j][1];
 					checkLS = false;
-					playing = false;
-					i = 3;
+					reg = false;
 					break;
 				}
-				else
-				{
-					positions[i] = positions[i]+nextRoll;
-					reg = false;
 				}
+				checkLS = false;
 			}
-    	}
-    }
-}
+				while(reg)
+				{
+					System.out.println("In regular");
+					//positions[i] = positions[i] + nextRoll;
+					if(positions[i]+nextRoll > 100) {
+						int excess = 100-(positions[i] + nextRoll);
+						System.out.println("*****OVER 100*****");
+						if(100+excess == 98 ) {
+							positions[i] = 78;
+							reg = false;
+						}
+						if(100+excess == 98) {
+							positions[i] = 76;
+							reg = false;
+						}
+						if(100+excess == 95) {
+							positions[i] = 24;
+							reg = false;
+						}
+						else {
+							positions[i] = 100+excess;
+							reg = false;
+						}
+					}
+					if(positions[i]+nextRoll==100 || positions[i]+nextRoll==80) {
+						positions[i] = 100;
+						System.out.println("Player " + i + " has won ");
+						reg = false;
+						checkLS = false;
+						playing = false;
+						i = 3;
+						break;
+					}
+					else
+					{
+						positions[i] = positions[i]+nextRoll;
+						reg = false;
+					}
+				}
+			}	
+		}	
+	}
 }
 
 
