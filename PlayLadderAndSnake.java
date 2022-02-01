1e53cf5fd5f228c708ac2c8c63d0a8fd32f64bef
baelise <94066054+baelise@users.noreply.github.com>
2022-02-01 17:24:40 -0500

    Create PlayLadderAndSnake.java

---
+++ PlayLadderAndSnake.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class PlayLadderAndSnakeRetry {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int players;
+        for(int attempt = 0; attempt < 4; attempt++ )
+        {
+        	System.out.println("Please Enter Numbers of Players (between 2and 4): ");
+            int userIn = scan.nextInt();
+            if (userIn >= 2 && userIn <=4){
+                players = userIn;
+                LadderAndSnakeRetry g1 = new LadderAndSnakeRetry(4);
+            	g1.play();
+                break;
+            }
+            else {
+                System.out.println("Invlaid!");
+            }
+        }
+        System.out.println("Game is terminated");
+    }
+}
