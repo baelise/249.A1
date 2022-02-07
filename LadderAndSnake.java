64bc87cb9d9819ea7b227f64cc91f5f5c782735c
baelise <94066054+baelise@users.noreply.github.com>
2022-02-07 00:00:34 -0500

    Update LadderAndSnake.java

--- LadderAndSnake.java
+++ LadderAndSnake.java
@@ -25,84 +25,90 @@ public class LadderAndSnake {
     	int roll = (int)(Math.random()*6+1);
     	return roll;
     }
-    public void play() {
-		boolean playing = true;
-		boolean checkLS = true;
-		boolean reg = true;
-		String[] order = {"Player 1", "Player 2", "Player 3", "Player 4"};
-		int[] positions = {0, 0, 0, 0};
-		while(playing) {
-			for(int i = 0; i < getPlayers(); i++) {
-				checkLS = true;
-			    reg = true;
-			    int nextRoll = flipDice();
-				System.out.println("\nRolling.......... "); 
-				System.out.println(order[i] + " rolls " + nextRoll);
-				while(checkLS) {
-		    		for(int j = 0; j <15; j++) {
-		    			if(positions[i]+nextRoll == board[j][0]) {
-		    				if(positions[i] < board[j][1]) {
-		    					System.out.println("Its a ladder! " + order[i] + " moves up to square " + board[j][1]);
-		    				}
-		    				else {
-		    					System.out.println("Oh no! Player " + order[i] + " moves down to square " + board[j][1]);
-		    				}		
-		    				positions[i] = board[j][1];	        			
-		        			checkLS = false;
-		            		reg = false;
-		        			break;
-		        		}
-		    		}
-		    		checkLS = false;
-				}
-				while(reg)
-				{
-					if(positions[i]+nextRoll > 100) {
-						int excess = 100-(positions[i] + nextRoll);
-						System.out.println("*****OVER 100*****");
-						if(100+excess == 98 ) {
-							positions[i] = 78;
-	    					System.out.println("Oh no! " + order[i] + " moves down to square " + positions[i]);
-							reg = false;
-							break;
-						}
-						if(100+excess == 98) {
-							positions[i] = 76;
-	    					System.out.println("Oh no! " + order[i] + " moves down to square " + positions[i]);
-							reg = false;
-							break;
-						}
-						if(100+excess == 95) {
-							positions[i] = 24;
-	    					System.out.println("Oh no! " + order[i] + " moves down to square " + positions[i]);
-							reg = false;
-							break;
-						}
-						else {
-							positions[i] = 100 + excess;
-	    					System.out.println(order[i] + " moves down to square " + positions[i]);
-							reg = false;
-							break;
-						}
-					}
-					if(positions[i]+nextRoll==100 || positions[i]+nextRoll==80) {
-						positions[i] = 100;
-						System.out.println(order[i] + " moves up to sqaure 100");
-						System.out.println(order[i] + " has won!");
-							reg = false;
-							checkLS = false;
-							playing = false;
-							i = 3;
-							break;
-						}
-					else
-					{
-						positions[i] = positions[i]+nextRoll;
-						System.out.println(order[i] + " moves up to square " + positions[i]);
-						reg = false;
-					}
-				}
-	    	}
-		}
-	}
+    public void play(String[] order) {
+        boolean playing = true;
+        boolean checkLS;
+        boolean reg;
+        int[] positions = {0, 0, 0, 0};
+        while(playing) {
+            for(int i = 0; i < getPlayers(); i++) {
+                checkLS = true;
+                reg = true;
+                int nextRoll = flipDice();
+                System.out.println("\nNEW PLAYER POSITIONS:");
+                for(int x = 0; x < getPlayers(); x++)
+                {
+                	System.out.println(order[x] + " is at sqaure " + positions[x] + "	");
+                }
+                System.out.println("\nRolling.......... ");
+                System.out.println(order[i] + " rolls " + nextRoll);
+                while(checkLS) {
+                    for(int j = 0; j <15; j++) {
+                        if(positions[i]+nextRoll == board[j][0]) {
+                            if(positions[i] < board[j][1]) {
+                                System.out.println(order[i] + " moves up to square " + (positions[i] + nextRoll));
+                                System.out.println("Its a ladder! " + order[i] + " moves up to square " + board[j][1]);
+                            }
+                            else {
+                                System.out.println(order[i] + " moves up to square " + (positions[i] + nextRoll));
+                                System.out.println("Oh no a snake! " + order[i] + " moves down to square " + board[j][1]);
+                            }
+                            positions[i] = board[j][1];
+                            reg = false;
+                            break;
+                        }
+                    }
+                    checkLS = false;
+                }
+                while(reg)
+                {
+                    if(positions[i]+nextRoll > 100) {
+                        int excess = 100-(positions[i] + nextRoll);
+                        if(100+excess == 98) {
+                            System.out.println(order[i] + " moves back to square 98");
+                            positions[i] = 78;
+                            System.out.println("Oh no a snake! " + order[i] + " moves down to square " + positions[i]);
+                            break;
+                        }
+                        if(100+excess == 97) {
+                            System.out.println(order[i] + " moves back to square 97");
+                            positions[i] = 76;
+                            System.out.println("Oh no a snake! " + order[i] + " moves down to square " + positions[i]);
+                            break;
+                        }
+                        if(100+excess == 95) {
+                            System.out.println(order[i] + " moves back to square 95");
+                            positions[i] = 24;
+                            System.out.println("Oh no a snake! " + order[i] + " moves down to square " + positions[i]);
+                        }
+                        else {
+                            positions[i] = 100 + excess;
+                            System.out.println(order[i] + " rolled over 100... moves back to square " + positions[i]);
+                        }
+                        break;
+                    }
+                    if(positions[i]+nextRoll==100 || positions[i]+nextRoll==80) {
+                    	if(positions[i]+nextRoll==80) {
+                    		System.out.println(order[i] + " moves up to square 80");
+                    		System.out.println("Its a ladder! " + order[i] + " moves up to square 100");
+                    	}
+                    	else {
+                            System.out.println(order[i] + " moves up to square 100");
+                    	}
+                        positions[i] = 100;
+                        System.out.println(order[i] + " has won!");
+                        playing = false;
+                        i = 4;
+                        break;
+                    }  
+                    else
+                    {
+                        positions[i] = positions[i]+nextRoll;
+                        System.out.println(order[i] + " moves up to square " + positions[i]);
+                        reg = false;
+                    }
+                }
+            }
+        }
+    }
 }
